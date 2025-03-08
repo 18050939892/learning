40a46025abdc41b29d5e3864d0113a766cc0a7e8
18050939892 <deerkesi3815@gmail.com>
2025-03-08 22:47:40 +0800

    https://github.com/18050939892/mmyzjreact.git

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import Context from './content.tsx'
+function App() {
+    // const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <Context></Context>
+        </>
+    )
+}
+
+export default App
