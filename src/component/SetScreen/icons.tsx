b91b16c02779e8094b37f09d6ec00c971bb51c12
18050939892 <deerkesi3815@gmail.com>
2025-03-15 17:08:26 +0800

    将js代码中的svg标签放入另一个组件文件，并将svg数组设置成组件的返回值，解决svg标签中带有原子实例和绑定方法的问题；将原子实例小驼峰命名；整理js代码，将相关代码整合在一起，并设置代码功能的备注

---
+++ src/component/SetScreen/icons.tsx
@@ -0,0 +1,40 @@
+import {  SetScrStyle } from '../../jotai/Store.ts'
+import { useAtom } from 'jotai'
+
+function icons() {
+    // 原子实例
+    const [setScrStyle, setSetScrStyle] = useAtom(SetScrStyle)
+    
+    // 设置界面关闭
+    const handleOnclick = (event) => {
+        setSetScrStyle({display: 'none', height: '0px'})
+        event.preventDefault()
+    }
+    
+     //   svg数组
+  const items= [
+      <svg
+          t="1741273897782" className="icon" viewBox="0 0 1024 1024" version="1.1"
+          xmlns="http://www.w3.org/2000/svg" p-id="4249" width="200" height="200"
+          onClick={handleOnclick}
+      >
+          <path
+              d="M550.848 502.496l308.64-308.896a31.968 31.968 0 1 0-45.248-45.248l-308.608 308.896-308.64-308.928a31.968 31.968 0 1 0-45.248 45.248l308.64 308.896-308.64 308.896a31.968 31.968 0 1 0 45.248 45.248l308.64-308.896 308.608 308.896a31.968 31.968 0 1 0 45.248-45.248l-308.64-308.864z"
+              p-id="4250" fill="#ffffff"
+          ></path>
+      </svg>,
+      <svg
+          t="1741273897782" className="icon" viewBox="0 0 1024 1024" version="1.1"
+          xmlns="http://www.w3.org/2000/svg" p-id="4249" width="200" height="200"
+          onClick={handleOnclick}
+      >
+          <path
+              d="M550.848 502.496l308.64-308.896a31.968 31.968 0 1 0-45.248-45.248l-308.608 308.896-308.64-308.928a31.968 31.968 0 1 0-45.248 45.248l308.64 308.896-308.64 308.896a31.968 31.968 0 1 0 45.248 45.248l308.64-308.896 308.608 308.896a31.968 31.968 0 1 0 45.248-45.248l-308.64-308.864z"
+              p-id="4250"
+          ></path>
+      </svg>
+  ]
+    return items
+}
+
+export default icons
