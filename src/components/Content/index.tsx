d7667f4131cf42adf84eebf78600ad9a5ba8ab80
sum <lineryforjs@gmail.com>
2025-03-22 16:23:22 +0800

    todo

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -5,7 +5,7 @@ import { SetScreen } from '../SetScreen'
 import { Footer } from '../Footer'
 import { Cover } from '../Cover'
 
-// 这里的 Content 的内容直接弄到 App.tsx 里，整个组件不要了。  属于无用的中间层
+// todo 这里的 Content 的内容直接弄到 App.tsx 里，整个组件不要了。  属于无用的中间层
 export function Content() {
     return (
         <div>
